3abb2784589d489f26b529d084baef0944a42996
Kenisha Hope <kenishahope12@gmail.com>
2024-05-14 22:01:25 -0500

    6.2

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-Use this repository to start class for the lesson 6.1
+# This is my class SPA
 
 
-The following packages will need to be installed to support the State-driven SPA using `npm install {package-name}`:
-html-literal
-parcel
-navigo (version 8)
-lodash
+
--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+import { header,nav,main,footer } from "./components";
+
+function render() {
+  document.querySelector("#root").innerHTML = `
+      ${header()}
+      ${nav()}
+      ${main()}
+      ${footer()}
+    `;
+  afterRender();
+}
+
+function afterRender() {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
+
+render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
